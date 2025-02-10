3dcc075bb2de761f0783c849a8232c8f37e003b6
[200~ <geetha-sekarr>
2025-02-10 18:22:08 +0530

    10-02-2025

--- logging.py
+++
@@ -1,8 +0,0 @@
-import logging
-logging.basicConfig(filename="c://unit test.py",filemode='w',)
-log=logging.getLogger()
-log.debug("this is debug")
-log.info("this is info")
-log.error("this is error")
-log.warning("this is warning log")
-log.critical("this is critical log")
